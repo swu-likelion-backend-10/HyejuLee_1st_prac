be8b63dd8e8a6f820dd48e0f1cb08b96142b5892
Hyeju02 <handaree02@naver.com>
2022-09-30 04:57:13 +0900

    3주차 두번째 과제

--- src/main/java/th_work/work/post/repository/BoardRepository.java
+++ src/main/java/th_work/work/post/repository/BoardRepository.java
@@ -1,7 +1,11 @@
 package th_work.work.post.repository;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import th_work.work.post.domain.Board;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
+    List<Board> findByNameContaining(String keyword);
 }
--- src/main/java/th_work/work/post/service/BoardService.java
+++ src/main/java/th_work/work/post/service/BoardService.java
@@ -75,4 +75,30 @@ public class BoardService {
     public void deletePost(Long id){
         boardRepository.deleteById(id);
     }
+
+    @Transactional
+    public List<BoardDto> searchPosts(String keyword) {
+        List<Board> boards = boardRepository.findByNameContaining(keyword);
+        List<BoardDto> boardDtoList = new ArrayList<>();
+
+        if (boards.isEmpty()) return boardDtoList;
+
+        for (Board board : boards) {
+            boardDtoList.add(this.convertEntityToDto(board));
+        }
+
+        return boardDtoList;
+    }
+
+    private BoardDto convertEntityToDto(Board board) {
+        return BoardDto.builder()
+                .id(board.getId())
+                .name(board.getName())
+                .major(board.getMajor())
+                .age(board.getAge())
+                .contents(board.getContents())
+                .createdTime(board.getCreatedTime())
+                .modifiedTime(board.getModifiedTime())
+                .build();
+    }
 }
